e4cb9874f53220ebf1de07f1284342f19599af60
Raiesh Ghimire <raieshg@gmail.com>
2020-11-17 17:34:14 +0545

    mobile app running

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:e_counter/Homepage.dart';
 import 'package:e_counter/districts_search.dart';
 import 'package:e_counter/login.dart';
 import 'package:e_counter/showvehicles.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main(){
-
-
-
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(new MaterialApp(
       home: e_counter(),
     ));}
