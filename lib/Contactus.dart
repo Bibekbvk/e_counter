2a7386243af20c69d9053e3bbac7cce6c3a56026
Raiesh Ghimire <raieshg@gmail.com>
2020-11-19 20:09:03 +0545

    contact us

--- lib/Contactus.dart
+++ lib/Contactus.dart
@@ -1,5 +1,6 @@
-import 'package:contactus/contactus.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:typicons_flutter/typicons_flutter.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -18,17 +19,39 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
 
-            ContactUs(
-              email: 'raieshgmail.com',
-              companyName: 'E-Counter',
-              phoneNumber: '+9779817931246',
+            displayCard("Ncell",Icons.call,"9860120715","tel:"),
+            displayCard("SmartCell", Icons.call, "9860120715", "tel:"),
+            displayCard("Ntc", Icons.call, "9860120715", "tel:"),
+            displayCard("Website", Typicons.link, "www.fb.com", ""),
+            displayCard("Facebook",Typicons.social_facebook, "facebook.com", ""),
 
-              tagLine: 'Contact Us For More Information',
-              facebookHandle: '',
-            ),
 
           ],
         ),
+      ),
+    );
+  }
+  Widget displayCard(String name,IconData icon,String id,String type){
+    return Card(
+      margin: EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 25.0,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      color: Colors.white,
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(
+          '$name',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        onTap: () {
+          launch('$type' + id);
+        },
       ),
     );
   }
