ea34e0db1b5ccd409c5f8236facb93f058e162c4
Bibekbvk <bhattaraibvk@gmail.com>
2020-11-26 13:16:33 +0545

    done

--- lib/Models/RentModel.dart
+++ lib/Models/RentModel.dart
@@ -1,18 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class RentModel {
   final String availability;
   final String capacity;
   final String currentlocation;
   final String insurance;
-  final String noofhelpers;
+  final String rentedby;
   final String pricing;
-  final String vehicleused;
+  final String vehicle_no;
   final String vehicle_id;
   final List price_list;
   final String vehicle_name;
+  final String description;
 
-  RentModel({this.availability, this.capacity, this.currentlocation, this.insurance, this.noofhelpers, this.pricing, this.vehicleused,this.vehicle_id,this.price_list, this.vehicle_name});
+  RentModel({this.availability, this.capacity, this.currentlocation, this.insurance, this.rentedby, this.pricing, this.vehicle_no,this.vehicle_id,this.price_list, this.vehicle_name, this.description});
 
 
 
@@ -24,10 +26,12 @@ class RentModel {
       capacity: data['capacity'],
       currentlocation: data['current_location'],
       insurance: data['insurance'],
-      noofhelpers: data['no_of_helper'],
+      rentedby: data['rentedby'],
       pricing: data['pricing'],
-      vehicleused: data['vehicle_used'],
+        vehicle_no: data['vehicle_no'],
       vehicle_id:data['vehicle_id'],
       price_list: data['price_list'],
-    vehicle_name: data['vehiclename']);}
+      vehicle_name: data['vehiclename'],
+      description: data['description']
+       );}
 }
